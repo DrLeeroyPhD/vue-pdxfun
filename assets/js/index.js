dae98b8a2da17041b1d10f4777aabb988857605c
Christopher Denny <dennyjchristopher@gmail.com>
2017-06-03 04:32:49 +0000

    Navigation Finished

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,19 @@
+Vue.component('navigation', {
+  template: 
+  `
+    <nav class="nav">
+        <div class="brand-img nav-left">
+          <img src="assets/img/pdxfun_logo.png" />
+        </div>
+        <div class="nav-right">
+          <a href="#" class="nav-item">Inventory</a>
+          <a href="#" class="nav-item">About</a>
+          <a href="#" class="nav-item">Reviews</a>
+        </div>
+      </nav>
+  `
+});
+
 
 new Vue({
    el: "#app" 
