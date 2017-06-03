6669bbeb867f72e5efc29880714cebf643acdc4f
Christopher Denny <dennyjchristopher@gmail.com>
2017-06-03 03:53:24 +0000

    Ditched Bulma, writing everything by hand. Nav finished (unresponsive)

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,37 +1,3 @@
-Vue.component('navigation', {
-    template: `
-    <nav class="nav has-shadow">
-        <div class="nav-left">
-          <a class="nav-item">
-            <img src="assets/img/pdxfun_logo.png" alt="PDX Fun logo" style="height: 90px;">
-          </a>
-        </div>
-      
-      
-        <!-- This "nav-toggle" hamburger menu is only visible on mobile -->
-        <!-- You need JavaScript to toggle the "is-active" class on "nav-menu" -->
-        <span class="nav-toggle">
-          <span></span>
-          <span></span>
-          <span></span>
-        </span>
-      
-        <!-- This "nav-menu" is hidden on mobile -->
-        <!-- Add the modifier "is-active" to display it on mobile -->
-        <div class="nav-right nav-menu">
-          <a class="nav-item">
-            Inventory
-          </a>
-          <a class="nav-item">
-            About
-          </a>
-          <a class="nav-item">
-            Reviews
-          </a>
-        </div>
-      </nav>
-    `
-});
 
 new Vue({
    el: "#app" 
